113d4f6de6c643ea115ce31d4cf77d3442e19818
ZacharyFolk <zacharyfolk@gmail.com>
2023-03-13 09:38:37 -0700

    Add validation methods

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const { validateUser, validate } = require('../middlewares/validator');
 const router = express.Router();
 const { createUser } = require('./../controllers/user');
 
-router.post('/create', createUser);
+router.post('/create', validateUser, validate, createUser);
 
 module.exports = router;
