dcabcb1b071ceca3d04a1f1c6e79b61bf3ecd759
ZacharyFolk <zacharyfolk@gmail.com>
2023-03-13 12:44:14 -0700

    Add bcrypt for password

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,11 +1,22 @@
 const User = require('./../model/user');
 
-exports.createUser = (req, res) => {
+exports.createUser = async (req, res) => {
   const { name, email, password } = req.body;
+  const user = await User.findOne({ email });
+  console.log('user ', user);
+
+  if (user) {
+    return res
+      .status(400)
+      .json({ success: false, error: 'This email already exists!' });
+  }
   const newUser = new User({
     name,
     email,
     password,
   });
+
+  await newUser.save();
+  console.log('User saved');
   res.send(newUser);
 };
--- api/model/user.js
+++ api/model/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -18,6 +19,16 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: '',
   },
+});
+
+// listen for save function and first run this callback function and hash password with bcrypt
+// only want to do this one time so use isModified
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    const hash = await bcrypt.hash(this.password, 8);
+    this.password = hash;
+  }
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
