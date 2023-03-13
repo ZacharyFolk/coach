62fb481aeae5c565721e7f336bf3ec32c00f0b55
ZacharyFolk <zacharyfolk@gmail.com>
2023-03-12 20:53:39 -0700

    Add error handler

--- api/app.js
+++ api/app.js
@@ -2,7 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// for middleware to parse the JSON payload and return req.body to next()
 app.use(express.json());
+// error handler for the middleware status 400 if syntax error in JSON otherwise 500 with error object
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    res.status(400).send({ error: 'Invalid JSON' });
+  } else {
+    res.status(500).send({ error: err.message });
+  }
+});
 
 app.post(
   '/api/user/create',
